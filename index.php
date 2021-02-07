5881a7444a6dc189689f08c6e4c84afee20423ea
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-07 20:47:39 +0500

    Rasmlar bilan page

--- index.php
+++ index.php
@@ -1,3 +1,55 @@
-<h3>Assalomu alaykum , bu video darslar code leader jamoasi tomonidan yaratildi</h3>
-git pul buyrug'i uchun maxsus buyruq
-uchinchi qator
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="http://s018.radikal.ru/i524/1710/40/97b1cf2b00ff.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://lh3.googleusercontent.com/proxy/tgso5Zh1V3vRYiblRt30nxxNcgU9okc5729u8HVKvpjeyR-4hkdjuXeUSzEavRzYzDIQoam53DP-2mwelGkk76Hf12vR" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ft.me%2Fs%2Fruhiyatmanzili%3Fbefore%3D134&psig=AOvVaw0EpLCpkjhjIT6CveiZsRa5&ust=1612798421817000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOC51rmM2O4CFQAAAAAdAAAAABAi" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+<br>
+<h1>TEPADAGI MADINA SANI KICHKINALIGINGDAGI RASMING</h1>
+<br>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="3.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="1.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
